b94ad304837af6b1a09cdfe92f730361c7cd9b5e
jkaho <72959522+jkaho@users.noreply.github.com>
2021-04-14 16:26:53 +1000

    Create Navbar component and add to app

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo-div">
+        <a className="navbar-logo" href="#">Employee Directory</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
