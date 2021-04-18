053c079b454f84b37c32e88afb5c063dde55cc3f
jkaho <72959522+jkaho@users.noreply.github.com>
2021-04-19 02:41:04 +1000

    Create Footer component and add to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Table from "./components/Table";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Table />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-link-div">
+        <a href="https://github.com/jkaho/employee-directory" target="_blank" rel="noopener noreferrer">Made by jkaho | 2021</a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
