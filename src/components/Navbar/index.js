c76d68424d7505be57ae41e7c75a242cca413570
jkaho <72959522+jkaho@users.noreply.github.com>
2021-04-19 02:35:24 +1000

    Deploy app to GitHub pages, move logo image to Navbar component folder

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import logo from './logo.png';
 import "./style.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-logo-div">
-        <img src="/images/logo.png" alt="Employee Directory logo" width="450"></img>
+        <img src={logo} alt="Employee Directory logo" width="450"></img>
       </div>
     </nav>
   )
